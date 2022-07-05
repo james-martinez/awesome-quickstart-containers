59149c5e89e521bb55fdec6e7249480d94927083
James Martinez <james-martinez@users.noreply.github.com>
2022-07-05 12:37:11 -0500

    Update run.sh

--- proxy-via-iptables/run.sh
+++ proxy-via-iptables/run.sh
@@ -9,8 +9,26 @@ sed -i "s/vPROXY-PORT/$PROXY_PORT/g" /etc/redsocks.conf
 
 echo "Restarting redsocks and redirecting traffic via iptables"
 /etc/init.d/redsocks restart
-iptables -t nat -A OUTPUT  -p tcp --dport 80 -j REDIRECT --to-port 12345
-iptables -t nat -A OUTPUT  -p tcp --dport 443 -j REDIRECT --to-port 12345
+iptables -t nat -N REDSOCKS
+iptables -t nat -A REDSOCKS -d 0.0.0.0/8 -j RETURN
+iptables -t nat -A REDSOCKS -d 10.0.0.0/8 -j RETURN
+iptables -t nat -A REDSOCKS -d 127.0.0.0/8 -j RETURN
+iptables -t nat -A REDSOCKS -d 169.254.0.0/16 -j RETURN
+iptables -t nat -A REDSOCKS -d 172.16.0.0/12 -j RETURN
+iptables -t nat -A REDSOCKS -d 192.168.0.0/16 -j RETURN
+iptables -t nat -A REDSOCKS -d 224.0.0.0/4 -j RETURN
+iptables -t nat -A REDSOCKS -d 240.0.0.0/4 -j RETURN
+
+iptables -t nat -A REDSOCKS -p tcp --dport 80 -j REDIRECT --to-port 12345
+iptables -t nat -A REDSOCKS -p tcp --dport 443 -j REDIRECT --to-port 12345
+iptables -t nat -A REDSOCKS -p tcp --dport 11371 -j REDIRECT --to-ports 12346
+
+iptables -t nat -A OUTPUT -p tcp -j REDSOCKS
+
+iptables -t nat -A PREROUTING -p tcp --dport 11371 -j REDSOCKS
+iptables -t nat -A PREROUTING -p tcp --dport 443 -j REDSOCKS
+iptables -t nat -A PREROUTING -p tcp --dport 80 -j REDSOCKS
+
 
 # Run app
 echo "Testing http access using: https://google.com "
